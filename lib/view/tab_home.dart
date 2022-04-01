0d03a9c328b8dedd99f80c5e438d8317af283077
Tasya <tasya.faziyah01@gmail.com>
2022-04-01 13:15:56 +0700

    updatetentangpage

--- lib/view/tab_home.dart
+++ lib/view/tab_home.dart
@@ -2,24 +2,23 @@ import 'package:flutter/material.dart';
 
 import 'beranda.dart';
 import 'statistik.dart';
-import 'tentang.dart';
+import 'tentangawal.dart';
 import 'kontak.dart';
 
 class TabTransitionPage extends StatefulWidget {
-  const TabTransitionPage({ Key? key }) : super(key: key);
+  const TabTransitionPage({Key? key}) : super(key: key);
 
   @override
   State<TabTransitionPage> createState() => _TabTransitionPageState();
 }
 
 class _TabTransitionPageState extends State<TabTransitionPage> {
-  
   int currIndex = 0;
 
   List<Widget> pages = [
     const BerandaPage(),
     const StatistikPage(),
-    const TentangPage(),
+    const TentangAwalPage(),
     const KontakPage(),
   ];
 
@@ -28,33 +27,33 @@ class _TabTransitionPageState extends State<TabTransitionPage> {
     return Scaffold(
       body: pages[currIndex],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currIndex,
-        onTap: (index) => setState(() {
-          currIndex = index;
-        }),
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(currIndex == 0 ? Icons.home : Icons.home_outlined),
-            label: 'Beranda',
-            backgroundColor: Colors.black,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(currIndex == 1 ? Icons.equalizer : Icons.equalizer_outlined),
-            label: 'Statistik',
-            backgroundColor: Colors.black,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(currIndex == 2 ? Icons.info : Icons.info_outline),
-            label: 'Tentang',
-            backgroundColor: Colors.black,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(currIndex == 3 ? Icons.call : Icons.call_outlined),
-            label: 'Kontak',
-            backgroundColor: Colors.black,
-          ),
-        ]
-      ),
+          currentIndex: currIndex,
+          onTap: (index) => setState(() {
+                currIndex = index;
+              }),
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(currIndex == 0 ? Icons.home : Icons.home_outlined),
+              label: 'Beranda',
+              backgroundColor: Colors.black,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                  currIndex == 1 ? Icons.equalizer : Icons.equalizer_outlined),
+              label: 'Statistik',
+              backgroundColor: Colors.black,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(currIndex == 2 ? Icons.info : Icons.info_outline),
+              label: 'Tentang',
+              backgroundColor: Colors.black,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(currIndex == 3 ? Icons.call : Icons.call_outlined),
+              label: 'Kontak',
+              backgroundColor: Colors.black,
+            ),
+          ]),
     );
   }
 }
---
+++ lib/view/tentangawal.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'tentang.dart';
+
+class TentangAwalPage extends StatefulWidget {
+  const TentangAwalPage({Key? key}) : super(key: key);
+
+  @override
+  State<TentangAwalPage> createState() => _TentangPageState();
+}
+
+class _TentangPageState extends State<TentangAwalPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tentang'),
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => TentangPage()));
+                },
+                child: const Text('UNIVERSITAS')),
+            const SizedBox(height: 12),
+            ElevatedButton(onPressed: () {}, child: const Text('FAKULTAS')),
+            const SizedBox(height: 12),
+            ElevatedButton(onPressed: () {}, child: const Text('DEPARTEMEN')),
+            const SizedBox(height: 12),
+            ElevatedButton(onPressed: () {}, child: const Text('PRODI')),
+          ],
+        ),
+      ),
+    );
+  }
+}
