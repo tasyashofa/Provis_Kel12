19949289d98e7863596c8204a80f1bf0579c96ee
Tasya <tasya.faziyah01@gmail.com>
2022-06-02 16:42:37 +0700

    update tentang_sejarah.dart

--- lib/view/tentang_sejarah_visi.dart
+++ lib/view/tentang_sejarah_visi.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SejarahVisiPage extends StatefulWidget {
-  const SejarahVisiPage({ Key? key }) : super(key: key);
+  const SejarahVisiPage({Key? key}) : super(key: key);
 
   @override
   State<SejarahVisiPage> createState() => _SejarahVisiPageState();
@@ -21,32 +21,50 @@ class _SejarahVisiPageState extends State<SejarahVisiPage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Card(
-              child: Padding(
-                padding: EdgeInsets.all(12),
-                child: Column(
-                  children: [
-                    Text('Visi Misi dan Sejarah'),
-                  ],
-                )
-              )
-            ),
-            Image.asset('assets/logo_upi.png',
+                child: Padding(
+                    padding: EdgeInsets.all(12),
+                    child: Column(
+                      children: [
+                        Text('Visi Misi dan Sejarah'),
+                      ],
+                    ))),
+            Image.asset(
+              'assets/logo_upi.png',
               width: 120,
               height: 120,
             ),
-            SizedBox(height: 12,),
-            Card(
-              child: SizedBox(
-                height: 100,
-              )
+            SizedBox(
+              height: 12,
             ),
-            SizedBox(height: 12,),
             Card(
-              child: SizedBox(
-                height: 100,
-              )
+                child: Padding(
+                    padding: EdgeInsets.all(12),
+                    child: Column(
+                      children: [
+                        Text('Visi'),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Text(
+                            'Pelapor dan Unggul dalam Pengembangan Kurikulum Program Pendidikan, Layanan Akademik, dan Jasa Keprofesian.'),
+                      ],
+                    ))),
+            SizedBox(
+              height: 12,
             ),
-            
+            Card(
+                child: Padding(
+                    padding: EdgeInsets.all(12),
+                    child: Column(
+                      children: [
+                        Text('Misi'),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Text(
+                            '1. Mengembangkan Kurikulum Program pendidikan akademik, vokasi, profesi, dan spesialis sesuai dengan standar yang ditetapkan. \n2. Mengoordinasikan penyelenggaraan program pendidikan akademik, vokasi, profesi dan spesialis yang transparan dan akuntabel. \n3. Menyelenggarakan seleksi dan layanan akademik program pendidikan akadaemik, vokasi, profesi dan spesialis yang berkualitas. \n4. Mengelola jasa keprofesian yang relevan dengan kebutuhan masyarakat.'),
+                      ],
+                    ))),
           ],
         ),
       ),
