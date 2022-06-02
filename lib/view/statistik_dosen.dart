ae776dbf926ee674c90fb84f78a8dc7c3ad1880b
Tasya <tasya.faziyah01@gmail.com>
2022-06-02 17:38:14 +0700

    update statistik dosen

--- lib/view/statistik_dosen.dart
+++ lib/view/statistik_dosen.dart
@@ -10,25 +10,25 @@ class StatistikDosenPage extends StatefulWidget {
 
 List<BarChartGroupData> barChartGroupData = [
   BarChartGroupData(x: 1, barRods: [
-    BarChartRodData(y: 5.5, colors: [Colors.black]),
+    BarChartRodData(y: 20, colors: [Colors.black]),
   ]),
   BarChartGroupData(x: 2, barRods: [
-    BarChartRodData(y: 12, colors: [Colors.black]),
+    BarChartRodData(y: 30, colors: [Colors.red]),
   ]),
   BarChartGroupData(x: 3, barRods: [
-    BarChartRodData(y: 8, colors: [Colors.black]),
+    BarChartRodData(y: 25, colors: [Colors.green]),
   ]),
   BarChartGroupData(x: 4, barRods: [
-    BarChartRodData(y: 11.4, colors: [Colors.black]),
+    BarChartRodData(y: 40, colors: [Colors.blue]),
   ]),
   BarChartGroupData(x: 5, barRods: [
-    BarChartRodData(y: 5, colors: [Colors.black]),
+    BarChartRodData(y: 50, colors: [Colors.yellow]),
   ]),
   BarChartGroupData(x: 6, barRods: [
-    BarChartRodData(y: 14, colors: [Colors.black]),
+    BarChartRodData(y: 23, colors: [Colors.orange]),
   ]),
   BarChartGroupData(x: 7, barRods: [
-    BarChartRodData(y: 10.5, colors: [Colors.black]),
+    BarChartRodData(y: 60, colors: [Colors.pink]),
   ]),
 ];
 
@@ -43,10 +43,15 @@ class _StatistikDosenPageState extends State<StatistikDosenPage> {
       body: SafeArea(
         child: Column(
           children: [
+            Padding(padding: EdgeInsets.all(10)),
+            Text(
+              "Statistik Jumlah Dosen Berdasarkan Fakultas",
+              textAlign: TextAlign.center,
+            ),
             Container(
                 padding: EdgeInsets.all(30),
                 width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.width,
+                height: 250,
                 child: BarChart(BarChartData(
                   titlesData: FlTitlesData(
                     topTitles: SideTitles(showTitles: false),
@@ -56,25 +61,25 @@ class _StatistikDosenPageState extends State<StatistikDosenPage> {
                       getTitles: (value) {
                         switch (value.toInt()) {
                           case 1:
-                            return 'Mon';
+                            return 'FPMIPA';
                           case 2:
-                            return 'Tues';
+                            return 'FIP';
                           case 3:
-                            return 'Wed';
+                            return 'FPIPS';
                           case 4:
-                            return 'Thu';
+                            return 'FPSD';
                           case 5:
-                            return 'Fri';
+                            return 'FPEB';
                           case 6:
-                            return 'Sat';
+                            return 'FPBS';
                           case 7:
-                            return 'Sun';
+                            return 'FPOK';
                         }
                         return "";
                       },
                     ),
                     leftTitles: SideTitles(
-                      interval: 4,
+                      interval: 20,
                       showTitles: true,
                       getTitles: (value) {
                         if (value.toInt() == 0)
@@ -84,7 +89,7 @@ class _StatistikDosenPageState extends State<StatistikDosenPage> {
                       },
                     ),
                   ),
-                  maxY: 16,
+                  maxY: 100,
                   borderData: FlBorderData(
                     show: false,
                   ),
--- lib/view/statistik_mahasiswa.dart
+++ lib/view/statistik_mahasiswa.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class StatistikMahasiswaPage extends StatefulWidget {
-  const StatistikMahasiswaPage({ Key? key }) : super(key: key);
+  const StatistikMahasiswaPage({Key? key}) : super(key: key);
 
   @override
   State<StatistikMahasiswaPage> createState() => _StatistikMahasiswaPageState();
@@ -20,56 +20,64 @@ class _StatistikMahasiswaPageState extends State<StatistikMahasiswaPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text("Statistik Mahasiswa", textAlign: TextAlign.center,),
-            SizedBox(height: 12,),
+            Text(
+              "Statistik Mahasiswa",
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 12,
+            ),
             Card(
               child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const ListTile(
-              title: Center(child: Text('Jenis Kelamin')),
-              subtitle: Center(child: Text('Jumlah mahasiswa berdasarkan jenis kelamin.')),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: Card(
-                    child: Padding(
-                      padding: EdgeInsets.all(12),
-                      child: ListTile(
-                        leading: Icon(Icons.man),
-                        title: Text('Lelaki'),
-                        subtitle: Text('7777'),
-                      ),
-                    ),
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const ListTile(
+                    title: Center(child: Text('Jenis Kelamin')),
+                    subtitle: Center(
+                        child: Text(
+                            'Jumlah mahasiswa berdasarkan jenis kelamin.')),
                   ),
-                ),
-                
-                Expanded(
-                  child: Card(
-                    child: Padding(
-                      padding: EdgeInsets.all(12),
-                      child: ListTile(
-                        leading: Icon(Icons.woman),
-                        title: Text('Wanita'),
-                        subtitle: Text('7676'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        child: Card(
+                          color: Color.fromARGB(255, 216, 237, 255),
+                          child: Padding(
+                            padding: EdgeInsets.all(5),
+                            child: ListTile(
+                              leading: Icon(Icons.man),
+                              title: Text('Lelaki'),
+                              subtitle: Text('7777'),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
+                      Expanded(
+                        child: Card(
+                          color: Color.fromARGB(255, 241, 214, 223),
+                          child: Padding(
+                            padding: EdgeInsets.all(5),
+                            child: ListTile(
+                              leading: Icon(Icons.woman),
+                              title: Text('Wanita'),
+                              subtitle: Text('7676'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ],
-        ),
+            SizedBox(
+              height: 12,
             ),
-            SizedBox(height: 12,),
             Card(
-              child: SizedBox(
-                height: 100,
-              )
-            ),
-            
+                child: SizedBox(
+              height: 100,
+            )),
           ],
         ),
       ),
