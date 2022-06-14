e6705d2848dbf02a81cbf74c1b6c383c12530bcb
fathiibrahimov <ahmadfathiibrahimov@gmail.com>
2022-06-14 15:47:35 +0700

    Update Chart di Dosen

--- lib/view/statistik_dosen.dart
+++ lib/view/statistik_dosen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:pie_chart/pie_chart.dart' as PieChart;
 import 'statistik_penelitian.dart';
 
 class StatistikDosenPage extends StatefulWidget {
@@ -34,6 +35,16 @@ List<BarChartGroupData> barChartGroupData = [
 ];
 
 class _StatistikDosenPageState extends State<StatistikDosenPage> {
+Map<String, double> dataMap = {
+    "S1": 40,
+    "S2": 25,
+  };
+
+ List<Color> colorList = [
+    const Color(0xff3EE094),
+    const Color(0xff3398F6),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,152 +52,193 @@ class _StatistikDosenPageState extends State<StatistikDosenPage> {
         title: const Text('Statistik Dosen'),
         foregroundColor: Colors.white,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            Container(
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                  Container(
-                    padding: EdgeInsets.all(5),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('   Pengabdian   '),
-                      style: ElevatedButton.styleFrom(
-                          textStyle: const TextStyle(fontSize: 10),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          )),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(5),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => PenelitianPage()));
-                      },
-                      child: const Text('   Penelitian  '),
-                      style: ElevatedButton.styleFrom(
-                          textStyle: const TextStyle(fontSize: 10),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          )),
-                    ),
-                  ),
-                ])),
-            Card(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const ListTile(
-                    title: Center(child: Text('Jenis Kelamin')),
-                    subtitle: Center(
-                        child: Text('Jumlah dosen berdasarkan jenis kelamin.')),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Expanded(
-                        child: Card(
-                          color: Color.fromARGB(255, 216, 237, 255),
-                          child: Padding(
-                            padding: EdgeInsets.all(5),
-                            child: ListTile(
-                              leading: Icon(Icons.man),
-                              title: Text('Lelaki'),
-                              subtitle: Text('7777'),
-                            ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Container(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('   Pengabdian   '),
+                            style: ElevatedButton.styleFrom(
+                                textStyle: const TextStyle(fontSize: 10),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                )),
                           ),
                         ),
-                      ),
-                      Expanded(
-                        child: Card(
-                          color: Color.fromARGB(255, 241, 214, 223),
-                          child: Padding(
-                            padding: EdgeInsets.all(5),
-                            child: ListTile(
-                              leading: Icon(Icons.woman),
-                              title: Text('Wanita'),
-                              subtitle: Text('7676'),
-                            ),
+                        Container(
+                          padding: EdgeInsets.all(5),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => PenelitianPage()));
+                            },
+                            child: const Text('   Penelitian  '),
+                            style: ElevatedButton.styleFrom(
+                                textStyle: const TextStyle(fontSize: 10),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                )),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Card(
-              child: Expanded(
+                  ])),
+              Card(
                 child: Column(
-                  children: [
-                    ListTile(
-                      title: Center(
-                          child: Text('Jumlah Dosen Berdasarkan Fakultas')),
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      title: Center(child: Text('Jenis Kelamin')),
+                      subtitle: Center(
+                          child: Text('Jumlah dosen berdasarkan jenis kelamin.')),
                     ),
-                    Container(
-                        padding: EdgeInsets.all(30),
-                        width: MediaQuery.of(context).size.width,
-                        height: 250,
-                        child: BarChart(BarChartData(
-                          titlesData: FlTitlesData(
-                            topTitles: SideTitles(showTitles: false),
-                            rightTitles: SideTitles(showTitles: false),
-                            bottomTitles: SideTitles(
-                              showTitles: true,
-                              getTitles: (value) {
-                                switch (value.toInt()) {
-                                  case 1:
-                                    return 'FPMIPA';
-                                  case 2:
-                                    return 'FIP';
-                                  case 3:
-                                    return 'FPIPS';
-                                  case 4:
-                                    return 'FPSD';
-                                  case 5:
-                                    return 'FPEB';
-                                  case 6:
-                                    return 'FPBS';
-                                  case 7:
-                                    return 'FPOK';
-                                }
-                                return "";
-                              },
-                            ),
-                            leftTitles: SideTitles(
-                              interval: 20,
-                              showTitles: true,
-                              getTitles: (value) {
-                                if (value.toInt() == 0)
-                                  return "";
-                                else
-                                  return value.toInt().toString();
-                              },
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Expanded(
+                          child: Card(
+                            color: Color.fromARGB(255, 216, 237, 255),
+                            child: Padding(
+                              padding: EdgeInsets.all(5),
+                              child: ListTile(
+                                leading: Icon(Icons.man),
+                                title: Text('Lelaki'),
+                                subtitle: Text('7777'),
+                              ),
                             ),
                           ),
-                          maxY: 100,
-                          borderData: FlBorderData(
-                            show: false,
+                        ),
+                        Expanded(
+                          child: Card(
+                            color: Color.fromARGB(255, 241, 214, 223),
+                            child: Padding(
+                              padding: EdgeInsets.all(5),
+                              child: ListTile(
+                                leading: Icon(Icons.woman),
+                                title: Text('Wanita'),
+                                subtitle: Text('7676'),
+                              ),
+                            ),
                           ),
-                          barGroups: barChartGroupData,
-                        )))
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 12,
+              ),
+              Card(
+                child: Expanded(
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: Center(
+                            child: Text('Jumlah Dosen Berdasarkan Fakultas')),
+                      ),
+                      Container(
+                          padding: EdgeInsets.all(30),
+                          width: MediaQuery.of(context).size.width,
+                          height: 250,
+                          child: BarChart(BarChartData(
+                            titlesData: FlTitlesData(
+                              topTitles: SideTitles(showTitles: false),
+                              rightTitles: SideTitles(showTitles: false),
+                              bottomTitles: SideTitles(
+                                showTitles: true,
+                                getTitles: (value) {
+                                  switch (value.toInt()) {
+                                    case 1:
+                                      return 'FPMIPA';
+                                    case 2:
+                                      return 'FIP';
+                                    case 3:
+                                      return 'FPIPS';
+                                    case 4:
+                                      return 'FPSD';
+                                    case 5:
+                                      return 'FPEB';
+                                    case 6:
+                                      return 'FPBS';
+                                    case 7:
+                                      return 'FPOK';
+                                  }
+                                  return "";
+                                },
+                              ),
+                              leftTitles: SideTitles(
+                                interval: 20,
+                                showTitles: true,
+                                getTitles: (value) {
+                                  if (value.toInt() == 0)
+                                    return "";
+                                  else
+                                    return value.toInt().toString();
+                                },
+                              ),
+                            ),
+                            maxY: 100,
+                            borderData: FlBorderData(
+                              show: false,
+                            ),
+                            barGroups: barChartGroupData,
+                          )))
+                    ],
+                  ),
+                ),
+              ),
+               SizedBox(
+                height: 12,
+              ),
+              Card(
+                child: Expanded(
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: Center(
+                            child: Text('Rasio Dosen Berdasarkan Jenjang Pendidikan Terakhir')),
+                      ),
+                      Container(
+                          padding: EdgeInsets.all(30),
+                          width: MediaQuery.of(context).size.width,
+                          height: 250,
+                          child: PieChart.PieChart(
+                              dataMap: dataMap,
+                              colorList: colorList,
+                              chartRadius: MediaQuery.of(context).size.width / 2,
+                              ringStrokeWidth: 32,
+                              animationDuration: const Duration(seconds: 3),
+                              chartValuesOptions: const PieChart.ChartValuesOptions(
+                                  showChartValues: true,
+                                  showChartValuesOutside: true,
+                                  showChartValuesInPercentage: true,
+                                  showChartValueBackground: false),
+                              legendOptions: const PieChart.LegendOptions(
+                                  showLegends: true,
+                                  legendShape: BoxShape.rectangle,
+                                  legendTextStyle: TextStyle(fontSize: 15),
+                                  legendPosition: PieChart.LegendPosition.right,
+                                  showLegendsInRow: false),
+                            ),
+                          )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+      )
+
     );
   }
 }
--- lib/view/statistik_penelitian.dart
+++ lib/view/statistik_penelitian.dart
@@ -39,22 +39,8 @@ class _PenelitianPageState extends State<PenelitianPage> {
     const Color(0xff3EE094),
     const Color(0xff3398F6),
     const Color(0xffFA4A42),
-    const Color(0xffFE9539)
-  ];
-
-  final gradientList = <List<Color>>[
-    [
-      Color.fromRGBO(223, 250, 92, 1),
-      Color.fromRGBO(129, 250, 112, 1),
-    ],
-    [
-      Color.fromRGBO(129, 182, 205, 1),
-      Color.fromRGBO(91, 253, 199, 1),
-    ],
-    [
-      Color.fromRGBO(175, 63, 62, 1.0),
-      Color.fromRGBO(254, 154, 92, 1),
-    ]
+    const Color(0xffFE9539),
+    Color.fromARGB(255, 251, 255, 0),
   ];
 
   @override
@@ -64,27 +50,43 @@ class _PenelitianPageState extends State<PenelitianPage> {
         title: const Text('Penelitian'),
         foregroundColor: Colors.white,
       ),
-      body: Center(
-        child: PieChart(
-          dataMap: dataMap,
-          colorList: colorList,
-          chartRadius: MediaQuery.of(context).size.width / 2,
-          centerText: "Jurnal",
-          ringStrokeWidth: 24,
-          animationDuration: const Duration(seconds: 3),
-          chartValuesOptions: const ChartValuesOptions(
-              showChartValues: true,
-              showChartValuesOutside: true,
-              showChartValuesInPercentage: true,
-              showChartValueBackground: false),
-          legendOptions: const LegendOptions(
-              showLegends: true,
-              legendShape: BoxShape.rectangle,
-              legendTextStyle: TextStyle(fontSize: 15),
-              legendPosition: LegendPosition.right,
-              showLegendsInRow: false),
-        ),
-      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Card(
+              child: Padding(padding: const EdgeInsets.all(12),
+                child: Column(
+                  children: [
+                    Text("Jumlah Jurnal Terakdreditasi", style: TextStyle(fontWeight: FontWeight.bold),),
+                    SizedBox(height: 30,),
+                    PieChart(
+                      dataMap: dataMap,
+                      colorList: colorList,
+                      chartRadius: MediaQuery.of(context).size.width / 2,
+                      centerText: "Jurnal",
+                      ringStrokeWidth: 32,
+                      animationDuration: const Duration(seconds: 3),
+                      chartValuesOptions: const ChartValuesOptions(
+                          showChartValues: true,
+                          showChartValuesOutside: true,
+                          showChartValuesInPercentage: true,
+                          showChartValueBackground: false),
+                      legendOptions: const LegendOptions(
+                          showLegends: true,
+                          legendShape: BoxShape.rectangle,
+                          legendTextStyle: TextStyle(fontSize: 15),
+                          legendPosition: LegendPosition.right,
+                          showLegendsInRow: false),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+          
+        )
+        )
     );
   }
 }
