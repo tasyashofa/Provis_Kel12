26880f96dd1c4eb957ceed5423dc4836bb84cc6d
Tasya <tasya.faziyah01@gmail.com>
2022-06-15 22:48:45 +0700

    update statistik mahasiswa

--- lib/view/statistik_mahasiswa.dart
+++ lib/view/statistik_mahasiswa.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:pie_chart/pie_chart.dart' as PieChart;
+import 'statistik_penelitian.dart';
 
 class StatistikMahasiswaPage extends StatefulWidget {
   const StatistikMahasiswaPage({Key? key}) : super(key: key);
@@ -33,131 +35,178 @@ List<BarChartGroupData> barChartGroupData = [
 ];
 
 class _StatistikMahasiswaPageState extends State<StatistikMahasiswaPage> {
+  Map<String, double> dataMap = {
+    "MBKM": 1000,
+    "Reguler": 7000,
+    "Cuti": 100,
+  };
+
+  List<Color> colorList = [
+    const Color(0xff3EE094),
+    const Color(0xff3398F6),
+    const Color(0xffFE9539),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Statistik Mahasiswa'),
-        foregroundColor: Colors.white,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(
-              height: 12,
-            ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(12),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    const ListTile(
-                      title: Center(child: Text('Jenis Kelamin')),
-                      subtitle: Center(
-                          child: Text(
-                              'Jumlah mahasiswa berdasarkan jenis kelamin.')),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Expanded(
-                          child: Card(
-                            color: Color.fromARGB(255, 216, 237, 255),
-                            child: Padding(
-                              padding: EdgeInsets.all(5),
-                              child: ListTile(
-                                leading: Icon(Icons.man),
-                                title: Text('Lelaki'),
-                                subtitle: Text('7777'),
+        appBar: AppBar(
+          title: const Text('Statistik Mahasiswa'),
+          foregroundColor: Colors.white,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const ListTile(
+                        title: Center(child: Text('Jenis Kelamin')),
+                        subtitle: Center(
+                            child: Text(
+                                'Jumlah mahasiswa berdasarkan jenis kelamin.')),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            child: Card(
+                              color: Color.fromARGB(255, 216, 237, 255),
+                              child: Padding(
+                                padding: EdgeInsets.all(5),
+                                child: ListTile(
+                                  leading: Icon(Icons.man),
+                                  title: Text('Lelaki'),
+                                  subtitle: Text('7777'),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        Expanded(
-                          child: Card(
-                            color: Color.fromARGB(255, 241, 214, 223),
-                            child: Padding(
-                              padding: EdgeInsets.all(5),
-                              child: ListTile(
-                                leading: Icon(Icons.woman),
-                                title: Text('Wanita'),
-                                subtitle: Text('7676'),
+                          Expanded(
+                            child: Card(
+                              color: Color.fromARGB(255, 241, 214, 223),
+                              child: Padding(
+                                padding: EdgeInsets.all(5),
+                                child: ListTile(
+                                  leading: Icon(Icons.woman),
+                                  title: Text('Wanita'),
+                                  subtitle: Text('7676'),
+                                ),
                               ),
                             ),
                           ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Card(
+                  child: Expanded(
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Center(
+                              child: Text(
+                                  'Jumlah mahasiswa Berdasarkan Fakultas')),
                         ),
+                        Container(
+                            padding: EdgeInsets.all(30),
+                            width: MediaQuery.of(context).size.width,
+                            height: 250,
+                            child: BarChart(BarChartData(
+                              titlesData: FlTitlesData(
+                                topTitles: SideTitles(showTitles: false),
+                                rightTitles: SideTitles(showTitles: false),
+                                bottomTitles: SideTitles(
+                                  showTitles: true,
+                                  getTitles: (value) {
+                                    switch (value.toInt()) {
+                                      case 1:
+                                        return 'FPMIPA';
+                                      case 2:
+                                        return 'FIP';
+                                      case 3:
+                                        return 'FPIPS';
+                                      case 4:
+                                        return 'FPSD';
+                                      case 5:
+                                        return 'FPEB';
+                                      case 6:
+                                        return 'FPBS';
+                                      case 7:
+                                        return 'FPOK';
+                                    }
+                                    return "";
+                                  },
+                                ),
+                                leftTitles: SideTitles(
+                                  interval: 20,
+                                  showTitles: true,
+                                  getTitles: (value) {
+                                    if (value.toInt() == 0)
+                                      return "";
+                                    else
+                                      return value.toInt().toString();
+                                  },
+                                ),
+                              ),
+                              maxY: 100,
+                              borderData: FlBorderData(
+                                show: false,
+                              ),
+                              barGroups: barChartGroupData,
+                            )))
                       ],
                     ),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Card(
-              child: Column(
-                children: [
-                  ListTile(
-                    title: Center(child: Text('Jumlah Mahasiswa Berdasarkan Fakultas')),
-                   
                   ),
-                  Container(
-                  padding: EdgeInsets.all(30),
-                  width: MediaQuery.of(context).size.width,
-                  height: 250,
-                  child: BarChart(BarChartData(
-                    titlesData: FlTitlesData(
-                      topTitles: SideTitles(showTitles: false),
-                      rightTitles: SideTitles(showTitles: false),
-                      bottomTitles: SideTitles(
-                        showTitles: true,
-                        getTitles: (value) {
-                          switch (value.toInt()) {
-                            case 1:
-                              return 'FPMIPA';
-                            case 2:
-                              return 'FIP';
-                            case 3:
-                              return 'FPIPS';
-                            case 4:
-                              return 'FPSD';
-                            case 5:
-                              return 'FPEB';
-                            case 6:
-                              return 'FPBS';
-                            case 7:
-                              return 'FPOK';
-                          }
-                          return "";
-                        },
-                      ),
-                      leftTitles: SideTitles(
-                        interval: 20,
-                        showTitles: true,
-                        getTitles: (value) {
-                          if (value.toInt() == 0)
-                            return "";
-                          else
-                            return value.toInt().toString();
-                        },
-                      ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Card(
+                  child: Expanded(
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Center(child: Text('Rasio Mahasiswa ')),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(30),
+                          width: MediaQuery.of(context).size.width,
+                          height: 250,
+                          child: PieChart.PieChart(
+                            dataMap: dataMap,
+                            colorList: colorList,
+                            chartRadius: MediaQuery.of(context).size.width / 2,
+                            ringStrokeWidth: 32,
+                            animationDuration: const Duration(seconds: 3),
+                            chartValuesOptions:
+                                const PieChart.ChartValuesOptions(
+                                    showChartValues: true,
+                                    showChartValuesOutside: true,
+                                    showChartValuesInPercentage: true,
+                                    showChartValueBackground: false),
+                            legendOptions: const PieChart.LegendOptions(
+                                showLegends: true,
+                                legendShape: BoxShape.rectangle,
+                                legendTextStyle: TextStyle(fontSize: 15),
+                                legendPosition: PieChart.LegendPosition.right,
+                                showLegendsInRow: false),
+                          ),
+                        )
+                      ],
                     ),
-                    maxY: 100,
-                    borderData: FlBorderData(
-                      show: false,
-                    ),
-                    barGroups: barChartGroupData,
-                  )))
-                ],
-              ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
